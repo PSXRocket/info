1939215538aad46b24716f06a974c9e378680b4b
PSXRocket <136690666+PSXRocket@users.noreply.github.com>
2023-07-02 15:00:44 +0800

    Create info.py

---
+++ info.py
@@ -0,0 +1,4 @@
+print(" (Hi, I’m @PSXRocket, I’m interested in basketball, coding, I’m currently learning python,lua,js,️ I’m looking to collaborate on discord bots, How to reach me: packski2 (discord))")
+
+
+(Hi, I’m @PSXRocket, I’m interested in basketball, coding, I’m currently learning python,lua,js,️ I’m looking to collaborate on discord bots, How to reach me: packski2 (discord))
